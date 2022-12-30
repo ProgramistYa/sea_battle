b066ee8795e2fbf2873520ef332bf95a5df49da3
lizard <s-ya98@ya.ru>
2022-12-30 20:54:59 +0300

    last_com

--- morscoi_boy.py
+++ morscoi_boy.py
@@ -3,6 +3,89 @@
 from random import randint
 #import emoji
 
+class Game:
+    def __init__(self, size=6):
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
+
+    def try_board(self):
+        lens = [3, 2, 2, 1, 1, 1, 1]
+        board = Board(size = self.size)
+        attempts = 0
+        for l in lens:
+            while True:
+                attempts += 1
+                if attempts > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), l, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except BoardWrongShipException:
+                    pass
+        board.begin()
+        return board
+
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.try_board()
+        return board
+
+    def greet(self):
+        print("%%%%%%%%%%%%%%%%%%%")
+        print("  Приветсвуем вас  ")
+        print("      в игре       ")
+        print("    морской бой    ")
+        print("%%%%%%%%%%%%%%%%%%%")
+        print(" формат ввода: X Y ")
+        print(" X - номер строки  ")
+        print(" Y - номер столбца ")
+
+    def print_boards(self):
+        print("-" * 20)
+        print("Доска пользователя:")
+        print(self.us.board)
+        print("-" * 20)
+        print("Доска компьютера:")
+        print(self.ai.board)
+        print("-" * 20)
+
+    def loop(self):
+        num = 0
+        while True:
+            self.print_boards()
+            if num % 2 == 0:
+                print("Ходит пользователь!")
+                repeat = self.us.move()
+            else:
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.count == 7:
+                self.print_boards()
+                print("-"*20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board.count == 7:
+                self.print_boards()
+                print("-"*20)
+                print("Компьютер выиграл")
+                break
+            num += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+
 class Dot:
     def __init__(self, x, y):
         self.x = x
@@ -171,92 +254,8 @@ class User(Player):
 
             return Dot(x-1, y-1)
 
-class Game:
-    def __init__(self, size=6):
-        self.size = size
-        pl = self.random_board()
-        co = self.random_board()
-        co.hid = False
-
-        self.ai = AI(co, pl)
-        self.us = User(pl, co)
-
-    def try_board(self):
-        lens = [3, 2, 2, 1, 1, 1, 1]
-        board = Board(size = self.size)
-        attempts = 0
-        for l in lens:
-            while True:
-                attempts += 1
-                if attempts > 2000:
-                    return None
-                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), l, randint(0, 1))
-                try:
-                    board.add_ship(ship)
-                    break
-                except BoardWrongShipException:
-                    pass
-        board.begin()
-        return board
-
-    def random_board(self):
-        board = None
-        while board is None:
-            board = self.try_board()
-        return board
-
-    def greet(self):
-        print("%%%%%%%%%%%%%%%%%%%")
-        print("  Приветсвуем вас  ")
-        print("      в игре       ")
-        print("    морской бой    ")
-        print("%%%%%%%%%%%%%%%%%%%")
-        print(" формат ввода: X Y ")
-        print(" X - номер строки  ")
-        print(" Y - номер столбца ")
-
-    def print_boards(self):
-        print("-" * 20)
-        print("Доска пользователя:")
-        print(self.us.board)
-        print("-" * 20)
-        print("Доска компьютера:")
-        print(self.ai.board)
-        print("-" * 20)
-
-    def loop(self):
-        num = 0
-        while True:
-            self.print_boards()
-            if num % 2 == 0:
-                print("Ходит пользователь!")
-                repeat = self.us.move()
-            else:
-                print("Ходит компьютер!")
-                repeat = self.ai.move()
-            if repeat:
-                num -= 1
-
-            if self.ai.board.count == 7:
-                self.print_boards()
-                print("-"*20)
-                print("Пользователь выиграл!")
-                break
-
-            if self.us.board.count == 7:
-                self.print_boards()
-                print("-"*20)
-                print("Компьютер выиграл")
-                break
-            num += 1
-
-    def start(self):
-        self.greet()
-        self.loop()
-
 g = Game()
 g.start()
-
 
 
 
